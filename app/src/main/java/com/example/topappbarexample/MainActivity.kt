6e5bbf0dead89d52002c4f4d2bad254869d40134
MikelWinemi <mikelwinemi@gmail.com>
2023-10-12 03:50:15 -0400

    initial commit

---
+++ app/src/main/java/com/example/topappbarexample/MainActivity.kt
@@ -0,0 +1,104 @@
+package com.example.topappbarexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.topappbarexample.ui.theme.TopAppBarExampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TopAppBarExampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TopAppBarExample()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBarExample() {
+    val  actionText = remember{
+        mutableStateOf("Actions will be shown here")
+    }
+
+    Scaffold(
+        topBar = {
+                 TopAppBar(
+                     navigationIcon = {
+                         IconButton(onClick = {actionText.value = "Navigation icon is clicked"}) {
+                             Icon(Icons.Filled.Menu, contentDescription = "")
+                         }
+                     },
+                     title = {
+                         Column {
+                             Text(text = "Top App Bar", color = Color.White, fontSize = 20.sp)
+                             Text(text = "Subtitle", color = Color.White, fontSize = 16.sp)
+                         }
+                     },
+                     actions = {
+                         IconButton(onClick = {actionText.value = "Share icon is clicked"}) {
+                             Icon(Icons.Filled.Share, contentDescription = "")
+                         }
+                         IconButton(onClick = {actionText.value = "Search icon is clicked"}) {
+                             Icon(Icons.Filled.Search, contentDescription = "")
+                         }
+                         IconButton(onClick = {actionText.value = "More options icon is clicked"}) {
+                             Icon(Icons.Filled.MoreVert, contentDescription = "")
+                         }
+                     },
+                     colors = TopAppBarDefaults.smallTopAppBarColors(
+                         containerColor = Color.Blue,
+                         navigationIconContentColor = Color.White,
+                         actionIconContentColor = Color.White
+                     )
+                 )
+
+        },
+        content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(it),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = actionText.value, color= Color.Companion.Black, fontSize = 18.sp)
+
+            }
+        }
+    )
+}
